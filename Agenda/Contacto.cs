f6ef54d2c371fb41e7da673710759b3d63dee957
imcasero <diegocaserosmr@gmail.com>
2023-04-11 17:26:31 +0200

    v1.0

---
+++ Agenda/Contacto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public static void AgregarContacto(Contacto nuevoContacto, List<Contacto> listaContactos)
+        {
+            listaContactos.Add(nuevoContacto);
+            Console.WriteLine("Contacto añadido correctamente");
+        }
+        public static void EliminarContacto(int id, List<Contacto> listaContactos)
+        {
+            bool borrado = false;
+            for (int i = 0; i < listaContactos.Count; i++)
+            {
+                if (listaContactos[i].Id == id)
+                {
+                    listaContactos.RemoveAt(i);
+                    borrado = true;
+                    break; // Salir del bucle una vez se ha eliminado el contacto
+                }
+            }
+
+            if (!borrado)
+            {
+                Console.WriteLine("No se ha encontrado el id seleccionado");
+            }
+            else
+            {
+                Console.WriteLine("Contacto eliminado correctamente");
+            }
+        }
+        public static Contacto BuscarContacto(int id, List<Contacto> listaContactos)
+        {
+            Contacto contacto = null;
+            foreach (Contacto c in listaContactos)
+            {
+                if (c.Id == id)
+                {
+                    contacto = c;
+                }
+            }
+            return contacto;
+
+        }
+        public static void EditarContacto( Contacto contacto)
+        {
+            Console.WriteLine("Inserte los datos que quiera modificar, en caso de no querer modificar ese campo escriba lo mismo a la anterior instancia");
+            Console.WriteLine("Inserte el nombre");
+            contacto.Nombre = Console.ReadLine();
+            Console.WriteLine("Inserte el Apellido");
+            contacto.Apellido = Console.ReadLine();
+            Console.WriteLine("Inserte la direccion");
+            contacto.Direccion = Console.ReadLine();
+            Console.WriteLine("Inserte el correo");
+            contacto.Correo = Console.ReadLine();
+            Console.WriteLine("Inserte el numero de telefono");
+            contacto.Telefono = Console.ReadLine();
+
+        }
+    }
+
+    
+
+}
---
+++ Agenda/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using CsvHelper;
+using System.IO;
+using System.Diagnostics.Contracts;
+//Crear una aplicación de gestión de contactos que permita almacenar, 
+//buscar, editar y eliminar información de contacto como nombre, apellido, 
+//dirección, correo electrónico y número de teléfono. La aplicación debe 
+//permitir la creación de múltiples agendas y la posibilidad de guardar y 
+//cargar la información de contacto en un archivo.
+namespace Agenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @".\Agenda\datos\contactos.csv";
+            bool respuesta = Comprobar(ruta);
+            List<Contacto> listaContactos = LeerContactosDesdeCSV(ruta);
+            if (respuesta)
+            {
+                bool continuar = true;
+
+                while (continuar)
+                {
+                    Console.WriteLine("Bienvenido a la Agenda");
+                    Console.WriteLine("Opciones:");
+                    Console.WriteLine("1. Ver contactos");
+                    Console.WriteLine("2. Añadir contacto");
+                    Console.WriteLine("3. Editar contacto");
+                    Console.WriteLine("4. Eliminar contacto");
+                    Console.WriteLine("5. Buscar contacto");
+                    Console.WriteLine("6. Salir");
+                    Console.Write("Seleccione una opción: ");
+                    int opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            foreach (Contacto c in listaContactos)
+                            {
+                                Console.WriteLine($"ID: {c.Id} , Nombre: {c.Nombre} , Apellido: {c.Apellido}");
+                            }
+                            break;
+                        case 2:
+                            Contacto nuevo = CrearContacto(listaContactos);
+                            Contacto.AgregarContacto(nuevo, listaContactos);
+                            EscribirContactosEnCSV(listaContactos , ruta);
+                            break;
+                        case 3:
+                            Console.WriteLine("Introduzca el id del contacto que desea editar");
+                            int idEdit = int.Parse(Console.ReadLine());
+                            Contacto contactoCambio = Contacto.BuscarContacto(idEdit, listaContactos);
+                            if (contactoCambio == null)
+                            {
+                                Console.WriteLine("No se encontro el contacto");
+                            }
+                            else
+                            {
+                                Contacto.EditarContacto(contactoCambio);
+                                EscribirContactosEnCSV(listaContactos, ruta);
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Introduzca el id que desea eliminar :");
+                            int idDelete = int.Parse(Console.ReadLine());
+                            Contacto.EliminarContacto(idDelete, listaContactos);
+                            EscribirContactosEnCSV(listaContactos, ruta);
+                            break;
+                        case 5:
+                            Console.WriteLine("Introduzca el id del contacto");
+                            int idSearch = int.Parse(Console.ReadLine());
+                            Contacto resul = Contacto.BuscarContacto(idSearch, listaContactos);
+                            if (resul == null)
+                            {
+                                Console.WriteLine("No se encontro el contacto");
+                            }else
+                            {
+                                Console.WriteLine("Id: " + resul.Id);
+                                Console.WriteLine("Nombre: " + resul.Nombre);
+                                Console.WriteLine("Apellido: " + resul.Apellido);
+                                Console.WriteLine("Direccion: " + resul.Direccion);
+                                Console.WriteLine("Correo: " + resul.Correo);
+                                Console.WriteLine("Telefono: " + resul.Telefono);
+                            }
+                            break;
+                        case 6:
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opción inválida. Por favor, seleccione una opción del menú.");
+                            break;
+                    }
+                }
+            }
+            else
+            { 
+                Console.WriteLine("No existen contactos, empeice a añadir contactos");
+            }
+        }
+        public static void EscribirContactosEnCSV(List<Contacto> contactos, string ruta)
+        {
+            using (StreamWriter sw = new StreamWriter(ruta))
+            {
+                foreach (Contacto c in contactos)
+                {
+                    string linea = $"{c.Id},{c.Nombre},{c.Apellido},{c.Direccion},{c.Correo},{c.Telefono}";
+                    sw.WriteLine(linea);
+                }
+            }
+        }
+        public static List<Contacto> LeerContactosDesdeCSV(string rutaArchivo)
+        {
+            List<Contacto> contactos = new List<Contacto>();
+
+            using (var reader = new StreamReader(rutaArchivo))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    Contacto contacto = new Contacto();
+                    contacto.Id = int.Parse(values[0]);
+                    contacto.Nombre = values[1];
+                    contacto.Apellido = values[2];
+                    contacto.Direccion = values[3];
+                    contacto.Correo = values[4];
+                    contacto.Telefono = values[5];
+
+                    contactos.Add(contacto);
+                }
+            }
+
+            return contactos;
+        }
+
+        public static Contacto CrearContacto(List<Contacto> listaContactos) 
+        {
+            int nuevoId = 1;
+            if (listaContactos.Count > 0)
+            {
+                nuevoId = listaContactos.Last().Id + 1;
+            }
+            Console.WriteLine("Introduzca el nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca el apellido");
+            string apellido = Console.ReadLine();
+            Console.WriteLine("Introduzca la direccion");
+            string direccion = Console.ReadLine();
+            Console.WriteLine("Introduzca el correo");
+            string correo = Console.ReadLine();
+            Console.WriteLine("Introduzca el numero de telefono");
+            string telefono = Console.ReadLine();
+            Contacto contacto = new Contacto() 
+            { 
+                Id = nuevoId,
+                Nombre = nombre,
+                Apellido = apellido,
+                Direccion = direccion,
+                Correo = correo,
+                Telefono = telefono,
+            };
+            return contacto;
+        }
+        public static bool Comprobar(string path)
+        { 
+            string[] lines = File.ReadAllLines(path);
+            if (lines.Length > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+}
